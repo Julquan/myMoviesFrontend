caa354b37d1737f096625d264bafb06b79b3d484
JulienQ <jquan84@gmail.com>
2023-01-31 14:35:35 +0100

    correction adresse backend

--- components/Home.js
+++ components/Home.js
@@ -1,30 +1,29 @@
-import { useState, useEffect } from 'react';
-import { Popover, Button } from 'antd';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
-import Movie from './Movie';
-import 'antd/dist/antd.css';
-import styles from '../styles/Home.module.css';
+import { useState, useEffect } from "react";
+import { Popover, Button } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import Movie from "./Movie";
+import "antd/dist/antd.css";
+import styles from "../styles/Home.module.css";
 
 function Home() {
-
   const [moviesData, setmoviesData] = useState([]);
 
   useEffect(() => {
-    fetch('https://my-movies-backend-sepia.vercel.app//movies')
-    .then(response => response.json())
-    .then(data => {setmoviesData(data.movies);
-      
-    });
-  },[]);
-  console.log(moviesData)
+    fetch("https://my-movies-backend-sepia.vercel.app/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        setmoviesData(data.movies);
+      });
+  }, []);
+  console.log(moviesData);
 
   const [likedMovies, setLikedMovies] = useState([]);
 
   // Liked movies (inverse data flow)
   const updateLikedMovies = (movieTitle) => {
-    if (likedMovies.find(movie => movie === movieTitle)) {
-      setLikedMovies(likedMovies.filter(movie => movie !== movieTitle));
+    if (likedMovies.find((movie) => movie === movieTitle)) {
+      setLikedMovies(likedMovies.filter((movie) => movie !== movieTitle));
     } else {
       setLikedMovies([...likedMovies, movieTitle]);
     }
@@ -34,15 +33,17 @@ function Home() {
     return (
       <div key={i} className={styles.likedMoviesContainer}>
         <span className="likedMovie">{data}</span>
-        <FontAwesomeIcon icon={faCircleXmark} onClick={() => updateLikedMovies(data)} className={styles.crossIcon} />
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          onClick={() => updateLikedMovies(data)}
+          className={styles.crossIcon}
+        />
       </div>
     );
   });
 
   const popoverContent = (
-    <div className={styles.popoverContent}>
-      {likedMoviesPopover}
-    </div>
+    <div className={styles.popoverContent}>{likedMoviesPopover}</div>
   );
 
   // Movies list
@@ -55,8 +56,19 @@ function Home() {
   // ];
 
   const movies = moviesData.map((data, i) => {
-    const isLiked = likedMovies.some(movie => movie === data.title);
-    return <Movie key={i} updateLikedMovies={updateLikedMovies} isLiked={isLiked} title={data.title} overview={`${data.overview.substring(0,250)}...`} poster={`https://image.tmdb.org/t/p/w500/${data.poster_path}`} voteAverage={data.vote_average} voteCount={data.vote_count} />;
+    const isLiked = likedMovies.some((movie) => movie === data.title);
+    return (
+      <Movie
+        key={i}
+        updateLikedMovies={updateLikedMovies}
+        isLiked={isLiked}
+        title={data.title}
+        overview={`${data.overview.substring(0, 250)}...`}
+        poster={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+        voteAverage={data.vote_average}
+        voteCount={data.vote_count}
+      />
+    );
   });
 
   return (
@@ -66,14 +78,17 @@ function Home() {
           <img src="logo.png" alt="Logo" />
           <img className={styles.logo} src="logoletter.png" alt="Letter logo" />
         </div>
-        <Popover title="Liked movies" content={popoverContent} className={styles.popover} trigger="click">
+        <Popover
+          title="Liked movies"
+          content={popoverContent}
+          className={styles.popover}
+          trigger="click"
+        >
           <Button>♥ {likedMovies.length} movie(s)</Button>
         </Popover>
       </div>
       <div className={styles.title}>LAST RELEASES</div>
-      <div className={styles.moviesContainer}>
-        {movies}
-      </div>
+      <div className={styles.moviesContainer}>{movies}</div>
     </div>
   );
 }
